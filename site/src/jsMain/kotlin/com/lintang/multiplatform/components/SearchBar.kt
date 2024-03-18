9936a308bdc85ee28f37059395bda48f0d1b6c98
lintang <lintangprayogo12@gmail.com>
2024-03-18 10:34:20 +0700

    popular-post

--- site/src/jsMain/kotlin/com/lintang/multiplatform/components/SearchBar.kt
+++ site/src/jsMain/kotlin/com/lintang/multiplatform/components/SearchBar.kt
@@ -5,10 +5,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import com.lintang.multiplatform.models.Theme
-import com.lintang.multiplatform.util.noBorder
 import com.varabyte.kobweb.compose.css.CSSTransition
-import com.varabyte.kobweb.compose.css.TransitionProperty
+import com.varabyte.kobweb.compose.css.Cursor
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
@@ -17,9 +15,13 @@ import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
 import com.varabyte.kobweb.compose.ui.modifiers.border
 import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
 import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.cursor
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.height
+import com.varabyte.kobweb.compose.ui.modifiers.id
 import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.onClick
 import com.varabyte.kobweb.compose.ui.modifiers.onFocusIn
 import com.varabyte.kobweb.compose.ui.modifiers.onFocusOut
 import com.varabyte.kobweb.compose.ui.modifiers.onKeyDown
@@ -29,62 +31,87 @@ import com.varabyte.kobweb.compose.ui.thenIf
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.icons.fa.FaMagnifyingGlass
 import com.varabyte.kobweb.silk.components.icons.fa.IconSize
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
 import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.css.LineStyle
 import org.jetbrains.compose.web.css.ms
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Input
+import com.lintang.multiplatform.models.Theme
+import com.lintang.multiplatform.util.Id
+import com.lintang.multiplatform.util.noBorder
 
 @Composable
 fun SearchBar(
+    breakpoint: Breakpoint,
+    modifier: Modifier = Modifier,
     fullWidth: Boolean = true,
-    onEnterClick: () -> Unit
+    darkTheme: Boolean = false,
+    onEnterClick: () -> Unit,
+    onSearchIconClick: (Boolean) -> Unit
 ) {
     var focused by remember { mutableStateOf(false) }
 
-    Row(
-        modifier = Modifier
-            .thenIf(
-                condition = fullWidth,
-                Modifier.fillMaxWidth()
-            ) 
-            .padding(left = 20.px)
-            .height(54.px)
-            .backgroundColor(Theme.LightGray.rgb)
-            .borderRadius(r = 130.px)
-            .border(
-                width = 2.px,
-                style = LineStyle.Solid,
-                color = if (focused) Theme.Primary.rgb else Theme.LightGray.rgb
-            ).transition(CSSTransition(property = TransitionProperty.All, duration = 200.ms)),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
+
+
+    if (breakpoint >= Breakpoint.SM || fullWidth) {
+        Row(
+            modifier = modifier
+                .thenIf(
+                    condition = fullWidth,
+                    other = Modifier.fillMaxWidth()
+                )
+                .padding(left = 20.px)
+                .height(54.px)
+                .backgroundColor(if (darkTheme) Theme.Tertiary.rgb else Theme.LightGray.rgb)
+                .borderRadius(r = 100.px)
+                .border(
+                    width = 2.px,
+                    style = LineStyle.Solid,
+                    color = if (focused && !darkTheme) Theme.Primary.rgb
+                    else if (focused && darkTheme) Theme.Primary.rgb
+                    else if (!focused && !darkTheme) Theme.LightGray.rgb
+                    else if (!focused && darkTheme) Theme.Secondary.rgb
+                    else Theme.LightGray.rgb
+                )
+                .transition(CSSTransition(property = "border", duration = 200.ms)),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            FaMagnifyingGlass(
+                modifier = Modifier
+                    .margin(right = 14.px)
+                    .color(if (focused) Theme.Primary.rgb else Theme.DarkGray.rgb)
+                    .transition(CSSTransition(property = "color", duration = 200.ms)),
+                size = IconSize.SM
+            )
+            Input(
+                type = InputType.Text,
+                attrs = Modifier
+                    .id(Id.adminSearchBar)
+                    .fillMaxSize()
+                    .color(if (darkTheme) Colors.White else Colors.Black)
+                    .backgroundColor(Colors.Transparent)
+                    .noBorder()
+                    .onFocusIn { focused = true }
+                    .onFocusOut { focused = false }
+                    .onKeyDown {
+                        if (it.key == "Enter") {
+                            onEnterClick()
+                        }
+                    }
+                    .toAttrs {
+                        attr("placeholder", "Search...")
+                    }
+            )
+        }
+    } else {
         FaMagnifyingGlass(
-            modifier =
-            Modifier.color(if (focused) Theme.Primary.rgb else Theme.DarkGray.rgb)
-                .margin(right = 14.px),
+            modifier = Modifier
+                .margin(right = 14.px)
+                .color(Theme.Primary.rgb)
+                .cursor(Cursor.Pointer)
+                .onClick { onSearchIconClick(true) },
             size = IconSize.SM
         )
-
-        Input(type = InputType.Text, attrs = Modifier
-            .fillMaxWidth()
-            .color(Colors.Black)
-            .onFocusIn {
-                focused = true
-            }
-            .onFocusOut {
-                focused = false
-            }
-            .backgroundColor(Colors.Transparent)
-            .noBorder()
-            .onKeyDown {
-                if (it.key == "Enter") {
-                    onEnterClick()
-                }
-            }
-            .toAttrs {
-                attr("placeholder", "Search...")
-            })
-
     }
 }
---
+++ site/src/jsMain/kotlin/com/lintang/multiplatform/section/PostSection.kt
@@ -0,0 +1,2 @@
+package com.lintang.multiplatform.section
+

01227d1d42f62267d83e63bec46a201ee514cbf3
lintang <lintangprayogo12@gmail.com>
2024-03-24 21:57:36 +0700

    fetch all post

---
+++ androidapp/src/main/java/com/lintang/androidapp/model/Post.kt
@@ -0,0 +1,18 @@
+package com.lintang.androidapp.model
+
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+
+
+open class Post : RealmObject {
+    @PrimaryKey
+    var _id: String = ""
+    var date: Long = 0
+    var title: String = ""
+    var subtitle: String = ""
+    var thumbnail: String = ""
+    var content: String = ""
+    var category: String = ""
+}
+
+
